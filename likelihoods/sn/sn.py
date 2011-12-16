63c4aab2b8e8093d62f458645f5c4390006a10c5
Julien Lesgourgues <lesgourg@mail.cern.ch>
2011-12-16 14:47:03 +0000

    bug corrected in sn.py

--- likelihoods/sn/sn.py
+++ likelihoods/sn/sn.py
@@ -4,46 +4,74 @@ from likelihood_class import likelihood
 
 class sn(likelihood):
   
+  # initialization routine
+
   def __init__(self,path,data,command_line=False):
 
     likelihood.__init__(self,path,data,command_line)
 
+    # define array for values of z and data points
     self.z	= np.array([],'float64')
     self.moduli = np.array([],'float64')
+
+    # read redshifts and data points
     for line in open(self.z_mu_dmu,'r'):
       self.z		= np.append(self.z,float(line.split()[1]))
-      self.moduli 	= np.append(self.z,float(line.split()[2]))
+      self.moduli 	= np.append(self.moduli,float(line.split()[2]))
 
+    # number of data points
+    self.num_points=np.shape(self.z)[0]
+
+    # define correlation m,atrix
+    covmat = np.zeros((self.num_points,self.num_points),'float64')
+
+    # file containing correlation matrix
     if self.has_syscovmat:
       covmat_filename = self.covmat_sys
     else:
       covmat_filename = self.covmat_nosys
 
-    covmat = np.zeros((np.shape(self.z)[0],np.shape(self.z)[0]),'float64')
+    # read correlation matrix
     i=0
     for line in open(covmat_filename,'r'):
       covmat[i] = line.split()
       i+=1
 
+    # invert correlation matrix
     self.inv_covmat=np.linalg.inv(covmat)
-    self.trace_inv_covmat=np.trace(self.inv_covmat)
 
+    # find sum of all matrix elements (sounds odd that there is 
+    # not a trace here instead, but this is correct!)
+    self.inv_covmat_sum=np.sum(self.inv_covmat)
+
+    # end of initialization
+
+  # compute likelihood
 
   def _loglkl(self,_cosmo,data):
 
-    difference = np.ndarray(np.shape(self.z)[0],'float64')
+    # define array for difference between theory and observations
+    difference = np.ndarray(self.num_points,'float64')
 
-    for i in range(np.shape(self.z)[0]):
+    # for each point, compute luminosity distance d_L=(1+z)**2d_A and infer
+    # theoretical prediction and difference with observation
+    for i in range(self.num_points):
       d = _cosmo._angular_distance(self.z[i])
-      #d = 2*self.z[i]
-      difference[i] = 5.0*np.log((1.+ self.z[i] )**2*d)/np.log(10) + 25.0 - self.moduli[i]
+      difference[i] = 5*np.log((1+self.z[i])**2*d)/np.log(10) + 25 - self.moduli[i]
 
-    AT = np.dot(np.transpose(difference),np.dot(self.inv_covmat,difference))
+    # chisquare before analytic marginalization
+    AT = np.dot(difference,np.dot(self.inv_covmat,difference))
+
+    # correct chi square with effect of analytic marginalization
     if self.has_marginalization:
       BT = np.sum(np.dot(self.inv_covmat,difference))
     else:
       BT = 0
 
-    chi_squared = AT - (BT**2)/self.trace_inv_covmat
+    # final chi square
+    chi_squared = AT - (BT**2)/self.inv_covmat_sum
+
+    # return ln(L)
     self.loglkl = - 0.5 * chi_squared 
+
     return self.loglkl
