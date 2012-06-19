fixed bug in changing columns for the input covariance, and changed display of covmatrices. BEWARE: the header has changed, so all old ones should either be recomputed, or modified in order to match the new format

code/analyze.py
@@ -1,8 +1,6 @@
 import os,sys
 import re
 import io
-import matplotlib.pyplot as plt
-import matplotlib
 import math
 import numpy as np
 
@@ -20,6 +18,9 @@ class info:
       self.has_interpolate_module = False
       print 'No cubic interpolation done (no interpolate method found in scipy), only linear'
 
+    if command_line.plot:
+      import matplotlib.pyplot as plt
+      import matplotlib
     # At this points, Files could contain either a list of files (that could be
     # only one) or a folder.
     Files     = command_line.files
@@ -68,11 +69,20 @@ class info:
 	if i!=j:
 	  self.covar[j][i]=self.covar[i][j]
 
-    self.cov.write('#{0}\n'.format(self.ref_names))
+    self.cov.write('# ')
+    for i in range(len(self.ref_names)):
+      string = self.ref_names[i]
+      if i != len(self.ref_names)-1:
+        string+=','
+      self.cov.write('%-16s' % string )
+    self.cov.write('\n')
     self.covar = np.dot(self.scales.T,np.dot(self.covar,self.scales))
     for i in range(len(self.ref_names)):
       for j in range(len(self.ref_names)):
-	self.cov.write('{0} '.format(self.covar[i][j]))
+        if self.covar[i][j]>0:
+          self.cov.write(' %.5e\t' % self.covar[i][j])
+        else:
+          self.cov.write('%.5e\t' % self.covar[i][j])
       self.cov.write('\n')
 
     # Sorting by likelihood

code/mcmc.py
@@ -97,7 +97,7 @@ def get_cov(data,command_line):
     cov=open('{0}'.format(command_line.cov),'r')
     for line in cov:
       if line.find('#')!=-1:
-	covnames = line.strip('#[').strip(']\n').replace("'","").split(', ')
+	covnames = line.strip('#').replace(' ','').replace('\n','').split(',')
 	M=np.zeros((len(covnames),len(covnames)),'float64')
 	rot=np.zeros((len(covnames),len(covnames)))
       else:
@@ -124,6 +124,7 @@ def get_cov(data,command_line):
     
     # Second print out, after having applied the scale factors
     print '\nFirst treatment (scaling)'
+    print covnames
     print M
 
     # Then, rotate M for the parameters to be well ordered, even if some names
@@ -139,7 +140,7 @@ def get_cov(data,command_line):
     not_in = [elem for elem in covnames if elem not in temp_names]
     for k in range(len(covnames)):
       if covnames[k] not in not_in:
-	temp_names_2.append(temp_names[h])
+        temp_names_2.append(temp_names[h])
 	h+=1
       else:
 	temp_names_2.append('')
@@ -153,10 +154,11 @@ def get_cov(data,command_line):
 	    rot[h][k] = 0.
 	except IndexError:
 	  rot[h][k] = 0.
-    M=np.dot(rot,np.dot(M,rot))
+    M=np.dot(rot,np.dot(M,np.transpose(rot)))
 
     # Third print out
     print '\nSecond treatment (partial reordering and cleaning)'
+    print temp_names_2
     print M
     
     M_temp    = np.ones((len(parameter_names),len(parameter_names)),'float64')