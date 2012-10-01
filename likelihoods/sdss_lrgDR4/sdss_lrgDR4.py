1554e3bea85bd7fb1bc6bc5c191d240ec5161b2e
Julien Lesgourgues <lesgourg@mail.cern.ch>
2012-10-01 12:11:15 +0000

    sdss updated to DR4 now, like in Tegmark paper

--- likelihoods/sdss_lrgDR4/sdss_lrgDR4.py
+++ likelihoods/sdss_lrgDR4/sdss_lrgDR4.py
@@ -1,6 +1,7 @@
 # simple likelihood for sdss_lrgDR4
 from likelihood_class import likelihood
 import numpy as np
+import math
 
 class sdss_lrgDR4(likelihood):
 
@@ -8,20 +9,21 @@ class sdss_lrgDR4(likelihood):
 
     likelihood.__init__(self,path,data,command_line,log_flag,default)
 
+    # require P(k) from class
     self.need_Class_arguments(data,{'output':'mPk', 'P_k_max_h/Mpc':2})
  
     # In case of a comparison, stop here
     if not default:
       return
 
-    # read values of k (in h/Mpc) and z
-    self.k_size=self.max_mpk_kbands_use-self.min_mpk_kbands_use+1
+    # in this likelihood, the P(k,z) is supposed to be evaluated only in z=0 
     self.z_size=1
-    self.mu_size=1
-
     self.z = np.zeros((self.z_size),'float64')
     self.z[0]=0
 
+    # read values of k (in h/Mpc)
+    self.k_size=self.max_mpk_kbands_use-self.min_mpk_kbands_use+1
+    self.mu_size=1
     self.k = np.zeros((self.k_size,self.z_size,self.mu_size),'float64')
     self.kh = np.zeros((self.k_size,self.z_size,self.mu_size),'float64')
 
@@ -29,7 +31,7 @@ class sdss_lrgDR4(likelihood):
     for i in range(self.num_mpk_kbands_full):
       line = datafile.readline()
       if i+2 > self.min_mpk_kbands_use and i < self.max_mpk_kbands_use:
-          self.kh[i-self.min_mpk_kbands_use+1,0,0]=float(line.split()[0])
+        self.kh[i-self.min_mpk_kbands_use+1,0,0]=float(line.split()[0])
     datafile.close()      
     
     # read window functions
@@ -65,35 +67,76 @@ class sdss_lrgDR4(likelihood):
 
   def loglkl(self,_cosmo,data):
 
-    # get values of k in 1/Mpc
-    self.k=self.kh *_cosmo._h()
+    # reduced Hubble parameter
+    h = _cosmo._h()
+    
+    # redhsift used in the rescaling
+    z_rescaling = 0.35
+ 
+    # angular diameter distance at this redshift, in Mpc/h
+    d_angular = _cosmo._angular_distance(z_rescaling)
+    d_angular *= h  
 
-    # get P(k) at right values of k and convert it to (Mpc/h)^3
-    P_th = np.zeros((self.k_size,self.z_size,self.mu_size),'float64')
-    P_th = _cosmo._get_pk(self.k,self.z,self.k_size,self.z_size,self.mu_size)
-    P_th /= (_cosmo._h())**3
+    # radial distance at this redshift, in Mpc/h
+    r,Hz = _cosmo.z_of_r([z_rescaling])
+    d_radial = z_rescaling*h/Hz[0]
 
-    # intermediate quantitites for chi2    
-    W_P_th =  np.zeros((self.n_size),'float64')
-    for i in range(self.n_size):
-      W_P_th[i]=0
-      for j in range(self.k_size):
-        W_P_th += self.window[i,j] * P_th[j,0,0]
+    # scaling factor = (d_angular**2 * d_radial)^(1/3) relative
+    # to a flat reference model with Omega_m=0.25, Omega_Lambda=0.75
+    scaling = pow((d_angular/722.4134)**2*(d_radial/897.9993),1./3.)
+    #scaling = pow((d_angular/1032.0191)**2*(d_radial/1282.8561),1./3.)
 
-    w = np.zeros((self.n_size),'float64') 
-    normV=0.
-    tmp=0.
-    for i in range(self.n_size):   
-      w[i] = 1/(self.P_err[i]*self.P_err[i])
-      normV += W_P_th[i]*W_P_th[i]*w[i]
-      tmp += W_P_th[i]*self.P_obs[i]*w[i]
-    tmp /= normV  
+    # get rescaled values of k in 1/Mpc
+    self.k=self.kh *h*scaling
 
-    # final chi2 expression (marginalized over bias)
-    chi2 = 0.
-    for i in range(self.n_size):
-      chi2 += self.P_obs[i]*(self.P_obs[i]-W_P_th[i]*tmp)*w[i]  
+    # get P(k) at right values of k, convert it to (Mpc/h)^3 and rescale it
+    P_lin = np.zeros((self.k_size,self.z_size,self.mu_size),'float64')
+    P_lin = _cosmo._get_pk(self.k,self.z,self.k_size,self.z_size,self.mu_size)
+    P_lin /= (h*scaling)**3
 
-    #print 'sdss chi2=',chi2  
+    do_marge = self.Q_marge
+
+    if do_marge and self.Q_flat:
+
+      P_th =  np.zeros((self.k_size),'float64')
+      for i in range(self.k_size):
+        P_th[i] = P_lin[i,0,0]/(1.+self.Ag*self.k[i]) 
+
+      k2 =  np.zeros((self.k_size),'float64')
+      for i in range(self.k_size):
+        k2[i] = P_th[i] * self.k[i]**2
+
+      W_P_th =  np.zeros((self.n_size),'float64')
+      W_P_th = np.dot(self.window,P_th)
+
+      W_P_th_k2 =  np.zeros((self.n_size),'float64')
+      W_P_th_k2 = np.dot(self.window,k2)
+                
+      w = np.zeros((self.n_size),'float64') 
+      w=1./(self.P_err*self.P_err)
+
+      covdat = np.zeros((self.n_size),'float64') 
+      covdat=self.P_obs*w  
+
+      covth = np.zeros((self.n_size),'float64') 
+      covth=W_P_th*w
+
+      covth_k2 = np.zeros((self.n_size),'float64') 
+      covth_k2=W_P_th_k2*w
+
+      offdiag=sum(covth*W_P_th_k2)
+
+      Mat=np.zeros((2,2),'float64')
+      Mat=[[sum(covth*W_P_th),offdiag],[offdiag,sum(covth_k2*W_P_th_k2)]]
+
+      Vec=np.zeros((2),'float64')
+      Vec=[sum(covdat*W_P_th),sum(covdat*W_P_th_k2)]
+
+      chi2=-sum(self.P_obs*covdat)+np.dot(Vec,np.dot(np.linalg.inv(Mat),Vec))-math.log(np.linalg.det(Mat))
+
+    else:  
+
+      print "case without marginalization and Q_flat not yet coded"
+      exit()
 
     return - chi2/2.
