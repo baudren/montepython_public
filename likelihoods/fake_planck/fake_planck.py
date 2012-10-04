3fa1bd222f639830c6fd06e04d93f47c02446878
Benjamin Audren <benj_audren@yahoo.fr>
2012-10-04 13:37:06 +0000

    removed fake_planck, old directory, from svn

--- likelihoods/fake_planck/fake_planck.py
+++
@@ -1,6 +0,0 @@
-# there is no specific likelihood code for this experiment, because it falls in the category of CMB experiments described in the "mock CMB" format. The class below inherits the properties of a general class "likelihood_mock_cmb", which knows how to deal with all experiments in "mock CMB" format.
-
-from likelihood_class import likelihood_mock_cmb
-
-class fake_planck(likelihood_mock_cmb):
-  pass
