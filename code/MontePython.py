3612b2a806b28a0ca2dc25549deb11f3c71a7b9e
Benjamin Audren <benj_audren@yahoo.fr>
2013-02-21 17:24:55 +0100

    MontePython, common and io_mp.py are PEP8

--- code/MontePython.py
+++ code/MontePython.py
@@ -7,147 +7,157 @@
 # Additional parts by Julien Lesgourgues, Karim Benabed
 ##############################################################
 
-#-------------------IMPORT-PACKAGES-------------------------------------
+#-------------------IMPORT-PACKAGES-------------------------------
 
 # Python modules
-import os,sys
+import os
+import sys
 
 # Checking for python version
 version = sys.version[:3]
 if float(version) < 2.7:
-  print '\n\n /|\  You must have Python >= 2.7,' 
-  print '/_o_\ or install manually the following modules for your older distribution:'
-  print '      argparse and OrderedDict (see MontePython.pdf)'
+    print '\n\n /|\  You must have Python >= 2.7,'
+    print '/_o_\ or install manually the following modules for your older'
+    print '      distribution: argparse and OrderedDict'
 
-import parser	# parsing the input command line
-import io_mp	# all the input/output mechanisms
-import mcmc	# the actual Monte Carlo chain procedure, along with the useful functions
-import data	# data handling
+import parser   # parsing the input command line
+import io_mp    # all the input/output mechanisms
+import mcmc     # the actual Monte Carlo chain procedure
+import data     # data handling
+
 
 #------------------MAIN-DEFINITION--------------------------------------
 def main():
-  # Parsing line argument
-  command_line=parser.parse()
+    # Parsing line argument
+    command_line = parser.parse()
 
-  # Default configuration
-  path = {}
-  # On execution, sys.path contains all the standard locations for the
-  # libraries, plus, on the first position (index 0), the directory from where
-  # the code is executed. By default, then, the data folder is located in the
-  # same root directory. Any setting in the configuration file will overwrite
-  # this one.
-  path['MontePython'] = sys.path[0]+'/'
-  path['data']        = path['MontePython'][:-5]+'data/'
+    # Default configuration
+    path = {}
 
-  # Configuration file, defaulting to default.conf in your root directory. This
-  # can be changed with the command line option -conf. All changes will be
-  # stored into the log.param of your folder, and hence will be reused for an
-  # ulterior run in the same directory
-  conf_file = path['MontePython']+'/../'+command_line.config_file
-  if os.path.isfile(conf_file):
-    for line in open(conf_file):
-      exec(line)
-    for key,value in path.iteritems():
-      if value[-1]!='/':
-	path[key] = value+'/'
-  else:
-    print ' /|\  You must provide a .conf file (default.conf by default)'
-    print '/_o_\ in your montepython directory that specifies'
-    print '      the correct locations for your data folder, Class (, Clik), ...'
+    # On execution, sys.path contains all the standard locations for the
+    # libraries, plus, on the first position (index 0), the directory from
+    # where the code is executed. By default, then, the data folder is located
+    # in the same root directory. Any setting in the configuration file will
+    # overwrite this one.
+    path['MontePython'] = sys.path[0] + '/'
+    path['data'] = path['MontePython'][:-5] + 'data/'
 
-  sys.stdout.write('Running MontePython version 1.1\n')
-
-  # If the info flag was used, read a potential chain (or set of chains) to be
-  # analysed with default procedure. If the argument is a .info file, then it
-  # will extract information from it (plots to compute, chains to analyse,
-  # etc...)
-  if command_line.files is not None:
-    import analyze	# analysis module, only invoked if asked in the command line
-    analyze.info(command_line)
-    exit()
-
-  # If the restart flag was used, load the cosmology directly from the
-  # log.param file, and append to the existing chain. 
-  if command_line.restart is not None:
-    if command_line.restart[0] == '/':
-      folder = ''
+    # Configuration file, defaulting to default.conf in your root directory.
+    # This can be changed with the command line option -conf. All changes will
+    # be stored into the log.param of your folder, and hence will be reused for
+    # an ulterior run in the same directory
+    conf_file = path['MontePython']+'/../' + command_line.config_file
+    if os.path.isfile(conf_file):
+        for line in open(conf_file):
+            exec(line)
+        for key, value in path.iteritems():
+            if not value.endswith('/'):
+                path[key] = value+'/'
     else:
-      folder = './'
-    for elem in command_line.restart.split("/")[:-1]:
-      folder += ''.join(elem+'/')
-    command_line.param = folder+'log.param'
-    command_line.folder = folder
-    sys.stdout.write('Reading {0} file'.format(command_line.restart))
-    Data = data.data(command_line,path)
+        print ' /|\  You must provide a .conf file (default.conf by default)'
+        print '/_o_\ in your montepython directory that specifies the correct'
+        print '      locations for your data folder, Class (, Clik), etc...'
 
-  # Else, fill in data, starting from  parameter file, If output folder already
-  # exists, first load a data instance with used param, and compare the two
-  # instances. If different, exit: you are not able to run two different things
-  # in one folder.
-  else:
-    Data=data.data(command_line,path)
-    if command_line.param.find('log.param')==-1:
-      try:
-        Data_old=data.data(command_line,path,False)
-        if Data!=Data_old:
-          print '\n /|\  You are starting a chain in {0} with different parameters\n/_o_\ than used previously.\n      Exiting'.format(command_line.folder)
-          exit()
-      except AttributeError:
-        pass
+    sys.stdout.write('Running MontePython version 1.1\n')
 
-  # Overwrite arguments from parameter file with the command line
-  if command_line.N is None:
-    try:
-      command_line.N=Data.N
-    except AttributeError:
-      print '\n /|\  You did not provide a number of steps,\n/_o_\ neither via command line, neither in {0}'.format(command_line.param)
-      exit()
+    # If the info flag was used, read a potential chain (or set of chains) to
+    # be analysed with default procedure. If the argument is a .info file, then
+    # it will extract information from it (plots to compute, chains to analyse,
+    # etc...)
+    if command_line.files is not None:
+        import analyze    # analysis module, only invoked when analyzing
+        analyze.info(command_line)
+        exit()
 
-  # Creating the file that will contain the chain
-  io_mp.create_output_files(command_line,Data)
+    # If the restart flag was used, load the cosmology directly from the
+    # log.param file, and append to the existing chain.
+    if command_line.restart is not None:
+        if command_line.restart[0] == '/':
+            folder = ''
+        else:
+            folder = './'
+        for elem in command_line.restart.split("/")[:-1]:
+            folder += ''.join(elem+'/')
+        command_line.param = folder+'log.param'
+        command_line.folder = folder
+        sys.stdout.write('Reading {0} file'.format(command_line.restart))
+        Data = data.data(command_line, path)
 
-  # Loading up the cosmological backbone. For the moment, only Class has been wrapped.
+    # Else, fill in data, starting from  parameter file, If output folder
+    # already exists, first load a data instance with used param, and compare
+    # the two instances. If different, exit: you are not able to run two
+    # different things in one folder.
+    else:
+        Data = data.data(command_line, path)
+        if command_line.param.find('log.param') == -1:
+            try:
+                Data_old = data.data(command_line, path, False)
+                if Data != Data_old:
+                    print '\n /|\  You are starting a chain in {0} with \
+                        different parameters\n/_o_\ than used previously.\n \
+                        Exiting'.format(command_line.folder)
+                    exit()
+            except AttributeError:
+                pass
 
-  #| Importing the python-wrapped Class from the correct folder, defined in the
-  #| .conf file, or overwritten at this point by the log.param
-  #| If there is a conflict between the log.param value and the .conf file, exiting.
-  if Data.path != path:
-    print ' /|\  Your log.param files is in contradiction with your .conf file,'
-    print '/_o_\ This might mean that you are not computing from the correct Class version'
-    print '      Exiting now'
-    exit()
+    # Overwrite arguments from parameter file with the command line
+    if command_line.N is None:
+        try:
+            command_line.N = Data.N
+        except AttributeError:
+            print '\n /|\  You did not provide a number of steps,\n/_o_\ \
+                neither via command line, neither in ',
+            print command_line.param
+            exit()
 
-  # If the cosmological code is Class, do the following to import all relevant quantities
-  if Data.cosmological_module_name == 'Class':
-    for elem in os.listdir(Data.path['cosmo']+"python/build"):
-      if elem.find("lib.") != -1:
-        classy_path = path['cosmo']+"python/build/"+elem
+    # Creating the file that will contain the chain
+    io_mp.create_output_files(command_line, Data)
 
-    #| Inserting the previously found path into the list of folders to search for
-    #| python modules.
-    sys.path.insert(1,classy_path)
-    try:
-      from classy import Class
-    except ImportError:
-      print(" /|\  You must have compiled the classy.pyx")
-      print("/_o_\ please go to /path/to/class/python and run")
-      print("      python setup.py build, then python setup.py install --user")
-      exit()
+    # Loading up the cosmological backbone. For the moment, only Class has been
+    # wrapped.
 
-    _cosmo=Class()
-  else:
-    print(" /|\  Error: unrecognised cosmological module")
-    print("/_o_\ Be sure to define the correct behaviour")
-    print("      in MontePython.py, and data.py")
-    exit()
+    # Importing the python-wrapped Class from the correct folder, defined in
+    # the .conf file, or overwritten at this point by the log.param.
+    # If there is a conflict between the log.param value and the .conf file,
+    # exiting.
+    if Data.path != path:
+        print ' /|\  Your log.param files is in contradiction with your .conf'
+        print '/_o_\ file. This might mean that you are not computing from the'
+        print '      correct Class version. Exiting now.'
+        exit()
 
-  # MCMC chain 
-  rate,min_LogLike=mcmc.chain(_cosmo,Data,command_line)
-  
-  # Closing up the file
-  Data.out.close()
+    # If the cosmological code is Class, do the following to import all
+    # relevant quantities
+    if Data.cosmological_module_name == 'Class':
+        for elem in os.listdir(Data.path['cosmo']+"python/build"):
+            if elem.find("lib.") != -1:
+                classy_path = path['cosmo']+"python/build/"+elem
+
+        # Inserting the previously found path into the list of folders to
+        # search for python modules.
+        sys.path.insert(1, classy_path)
+        try:
+            from classy import Class
+        except ImportError:
+            print(" /|\  You must have compiled the classy.pyx")
+            print("/_o_\ please go to /path/to/class/python and run")
+            print("      python setup.py build")
+            exit()
+
+        cosmo = Class()
+    else:
+        print(" /|\  Error: unrecognised cosmological module")
+        print("/_o_\ Be sure to define the correct behaviour")
+        print("      in MontePython.py, and data.py")
+        exit()
+
+    # MCMC chain
+    rate, min_LogLike = mcmc.chain(cosmo, Data, command_line)
+
+    # Closing up the file
+    Data.out.close()
 
 
 #-----------------MAIN-CALL---------------------------------------------
 if __name__ == '__main__':
-  main()
+    main()
--- code/io_mp.py
+++ code/io_mp.py
@@ -1,104 +1,125 @@
-# This module will handle all the input/output of the code (at least most of
-# it). 
+"""
+Input-Output handling
 
-# So if something is printed that does not satisfy you (number of decimals, for
-# instance), you only have to find the called function and change a number.
+Handles all the input/output of the code (at least most of it). If something is
+printed that does not satisfy you (number of decimals, for instance, in the
+output files), you only have to find the called function and change a number.
 
-import os,sys
-import re     # Module to handle regular expressions (powerful tool to manipulate strings)
+"""
+
+import os
+import sys
+import re     # Module to handle regular expressions
 import random as rd
 import numpy as np
 try:
-  from collections import OrderedDict as od
+    from collections import OrderedDict as od
 except:
-  from ordereddict import OrderedDict as od    
+    from ordereddict import OrderedDict as od
 from datetime import date
-from common import lock,LockException
+from common import lock, LockException
 import fcntl
 
-# Writes the beginning of log.param, starting with the header with the
-# cosmological code version and subversion, and then recopies entirely the
-# input parameter file
-def log_parameters(data,command_line):
-  log     = open(command_line.folder+'/log.param','w')
-  param_file = open(command_line.param,'r')
-  log.write("#-----{0} {1} (subversion {2})-----\n\n".format(data.cosmological_module_name,data.version,data.subversion))
-  for line in param_file:
-    log.write(line)
-  param_file.close()
-  log.close()
 
-# Writes the configuration for each and every likelihood used
-def log_likelihood_parameters(likelihood,command_line):
-  log = open(command_line.folder+'log.param','a')
-  tolog = open(likelihood.path,'r')
-  log.write("\n\n#-----Likelihood-{0}-----\n".format(likelihood.name))
-  for line in tolog:
-    log.write(line)
-  tolog.seek(0)
-  tolog.close()
-  log.close()
+def log_parameters(data, command_line):
+    """
+    Write the first lines of the log.param
+
+    Writes the beginning of log.param, starting with the header with the
+    cosmological code version and subversion, and then recopies entirely the
+    input parameter file.
+
+    """
+    log = open(command_line.folder+'/log.param', 'w')
+    param_file = open(command_line.param, 'r')
+    log.write("#-----{0} {1} (subversion {2})-----\n\n".format(
+        data.cosmological_module_name, data.version, data.subversion))
+    for line in param_file:
+        log.write(line)
+    param_file.close()
+    log.close()
+
+
+def log_likelihood_parameters(likelihood, command_line):
+    """Write down the .data file of the input likelihood to log.param"""
+    log = open(command_line.folder+'log.param', 'a')
+    tolog = open(likelihood.path, 'r')
+    log.write("\n\n#-----Likelihood-{0}-----\n".format(likelihood.name))
+    for line in tolog:
+        log.write(line)
+    tolog.seek(0)
+    tolog.close()
+    log.close()
+
 
 # Third function called when writing log.param, it writes down the
 # cosmo_arguments used (it is understood here that all the other parameters for
 # the cosmological modules are set to their default value directly in the
 # program). It is written as an update for the dictionary cosmo_arguments, in
 # order not to erase previously initialized data.
-def log_cosmo_arguments(data,command_line):
-  if len(data.cosmo_arguments) >= 1:
-    log     = open(command_line.folder+'/log.param','a')
-    log.write('\n\n#-----------Cosmological-arguments---------\n')
-    log.write('data.cosmo_arguments.update({0})\n'.format(data.cosmo_arguments))
-    log.close()
+def log_cosmo_arguments(data, command_line):
+    if len(data.cosmo_arguments) >= 1:
+        log = open(command_line.folder+'/log.param', 'a')
+        log.write('\n\n#-----------Cosmological-arguments---------\n')
+        log.write('data.cosmo_arguments.update({0})\n'.format(
+            data.cosmo_arguments))
+        log.close()
+
 
 # Fourth and last function called when writing log.param, it logs the .conf
 # file used to get the path. Only useful if you have several versions of your
 # cosmological code installed in different locations, or different versions of
 # Clik. But, as you never know what might go wrong, it is logged everytime !
-def log_default_configuration(data,command_line):
-  log = open(command_line.folder+'/log.param','a')
-  log.write('\n\n#--------Default-Configuration------\n')
-  for key,value in data.path.iteritems():
-    log.write("data.path['{0}']\t= '{1}'\n".format(key,value))
-  log.close()
+def log_default_configuration(data, command_line):
+    log = open(command_line.folder+'/log.param', 'a')
+    log.write('\n\n#--------Default-Configuration------\n')
+    for key, value in data.path.iteritems():
+        log.write("data.path['{0}']\t= '{1}'\n".format(key, value))
+    log.close()
+
 
 # Will print the parameter names. In the code, out is simply the standard
 # output, as this information will not be printed on the output file. Indeed,
 # you will be able to recover these information from the log.param. Please pay
 # attention to the fact that, once launched, the order of the parameters in
 # log.param is crucial, as is it the only place where it is stored.
-def print_parameters(out,data):
-  param = data.get_mcmc_parameters(['varying'])
-  for elem in data.get_mcmc_parameters(['derived']):
-    param.append(elem)
-  out.write('\n#  -LogLkl\t')
-  for i in range(len(param)):
-    if data.mcmc_parameters[param[i]]['scale']!=1:
-      number = data.mcmc_parameters[param[i]]['scale']
-      if (number > 100. or number < 0.01):
-	string = '%0.e%s' % (1./number,param[i])
-      else:
-	string = '%0.2g%s' % (1./number,param[i])
-    else:
-      string = '%s' % param[i]
-    out.write("%-16s" % string)
-  out.write('\n')
+def print_parameters(out, data):
+    param = data.get_mcmc_parameters(['varying'])
+    for elem in data.get_mcmc_parameters(['derived']):
+        param.append(elem)
+    out.write('\n#  -LogLkl\t')
+    for i in range(len(param)):
+        if data.mcmc_parameters[param[i]]['scale'] != 1:
+            number = data.mcmc_parameters[param[i]]['scale']
+            if (number > 100. or number < 0.01):
+                string = '%0.e%s' % (1./number, param[i])
+            else:
+                string = '%0.2g%s' % (1./number, param[i])
+        else:
+            string = '%s' % param[i]
+        out.write("%-16s" % string)
+    out.write('\n')
+
 
 # Prints the last accepted values to out, which here is an array containing
 # both standard output and the output file. This way, if you run in interactive
 # mode, you will be able to monitor the progress of the chain.
-def print_vector(out,N,loglkl,data):
-  for j in range(len(out)):
-    out[j].write('%d  %.6g\t' % (N,-loglkl))
-    for elem in data.get_mcmc_parameters(['varying']):
-      out[j].write('%.6e\t' % data.mcmc_parameters[elem]['last_accepted'])
-    for elem in data.get_mcmc_parameters(['derived']):
-      out[j].write('%.6e\t' % data.mcmc_parameters[elem]['last_accepted'])
-    out[j].write('\n')
+def print_vector(out, N, loglkl, data):
+    for j in range(len(out)):
+        out[j].write('%d  %.6g\t' % (N, -loglkl))
+        for elem in data.get_mcmc_parameters(['varying']):
+            out[j].write('%.6e\t' %
+                         data.mcmc_parameters[elem]['last_accepted'])
+        for elem in data.get_mcmc_parameters(['derived']):
+            out[j].write('%.6e\t' %
+                         data.mcmc_parameters[elem]['last_accepted'])
+        out[j].write('\n')
+
 
 def refresh_file(data):
-  data.out.close()
-  data.out=open(data.out_name,'a')
+    data.out.close()
+    data.out = open(data.out_name, 'a')
+
 
 # This routine takes care of organising the folder for you. It will
 # automatically generate names for the new chains according to the date, number
@@ -109,81 +130,91 @@ def refresh_file(data):
 # The way these names are generated (with the proper number of _, __, -, and
 # their placement) is exploited in the rest of the code in various places.
 # Please keep that in mind if ever you are in the mood of changing things here.
-def create_output_files(command_line,data):
-  if command_line.restart is None:
-    number = command_line.N
-  else:
-    number = int(command_line.restart.split('/')[-1].split('__')[0].split('_')[1]) + command_line.N
+def create_output_files(command_line, data):
+    if command_line.restart is None:
+        number = command_line.N
+    else:
+        number = int(command_line.restart.split('/')[-1].split('__')[0].
+                     split('_')[1]) + command_line.N
 
-  # output file
-  outname_base='{0}_{1}__'.format(date.today(),number)
-  suffix=0
-  Try = True
-  if command_line.chain_number is None:
-    for files in os.listdir(command_line.folder):
-      if files.find(outname_base)!=-1:
-        if int(files.split('__')[-1].split('.')[0])>suffix:
-          suffix=int(files.split('__')[-1].split('.')[0])
-    suffix+=1
-    while Try:
-      data.out = open(command_line.folder+outname_base+str(suffix)+'.txt','w')
-      try:
-        lock(data.out, fcntl.LOCK_EX | fcntl.LOCK_NB)
-        Try = False
-      except LockException:
-        suffix+=1
-    sys.stdout.write('Creating {0}{1}{2}.txt\n'.format(command_line.folder,outname_base,suffix))
-    data.out_name='{0}{1}{2}.txt'.format(command_line.folder,outname_base,suffix)
-  else:
-    data.out=open(command_line.folder+outname_base+command_line.chain_number+'.txt','w')
-    sys.stdout.write('Creating {0}{1}{2}.txt\n'.format(command_line.folder,outname_base,command_line.chain_number))
-    data.out_name='{0}{1}{2}.txt'.format(command_line.folder,outname_base,command_line.chain_number)
-  # in case of a restart, copying the whole thing in the new file
-  if command_line.restart is not None:
-    for line in open(command_line.restart,'r'):
-      data.out.write(line)
+    # output file
+    outname_base = '{0}_{1}__'.format(date.today(), number)
+    suffix = 0
+    Try = True
+    if command_line.chain_number is None:
+        for files in os.listdir(command_line.folder):
+            if files.find(outname_base) != -1:
+                if int(files.split('__')[-1].split('.')[0]) > suffix:
+                    suffix = int(files.split('__')[-1].split('.')[0])
+        suffix += 1
+        while Try:
+            data.out = open(command_line.folder+outname_base +
+                            str(suffix)+'.txt', 'w')
+            try:
+                lock(data.out, fcntl.LOCK_EX | fcntl.LOCK_NB)
+                Try = False
+            except LockException:
+                suffix += 1
+        sys.stdout.write('Creating {0}{1}{2}.txt\n'.format(
+            command_line.folder, outname_base, suffix))
+        data.out_name = '{0}{1}{2}.txt'.format(
+            command_line.folder, outname_base, suffix)
+    else:
+        data.out = open(command_line.folder+outname_base +
+                        command_line.chain_number+'.txt', 'w')
+        sys.stdout.write('Creating {0}{1}{2}.txt\n'.format(
+            command_line.folder, outname_base, command_line.chain_number))
+        data.out_name = '{0}{1}{2}.txt'.format(
+            command_line.folder, outname_base, command_line.chain_number)
+    # in case of a restart, copying the whole thing in the new file
+    if command_line.restart is not None:
+        for line in open(command_line.restart, 'r'):
+            data.out.write(line)
 
-# Simple tex name transformer. 
-def get_tex_name(name,number=1):
-  tex_greek = ['omega','tau','alpha','beta','delta','nu','Omega','Lambda','lambda']
-  for elem in tex_greek:
-    if elem in name:
-      position = name.find(elem)
-      name=name[:position]+"""\\"""+name[position:]
-  if name.find('_')!=-1:
-    temp_name = name.split('_')[0]+'_{'
-    for i in range(1,len(name.split('_'))):
-      temp_name += name.split('_')[i]+' '
-    name = temp_name + '}'
-  if number==1: 
-    name = "${0}$".format(name)
-  elif (number < 1000 and number > 1):
-    name = "$%0.d~%s$" % (number,name)
-  else:
-    temp_name = "$%0.e%s$" % (number,name)
-    m = re.search(r'(?:\$[0-9]*e\+[0]*)([0-9]*)(.*)',temp_name)
-    sign = '+'
-    if m == None:
-      m = re.search(r'(?:\$[0-9]*e\-[0]*)([0-9]*)(.*)',temp_name)
-      sign = '-'
-    name = '$10^{'+sign+m.groups()[0]+'}'+m.groups()[1]
-  return name
+
+# Simple tex name transformer.
+def get_tex_name(name, number=1):
+    tex_greek = ['omega', 'tau', 'alpha', 'beta', 'delta', 'nu',
+                 'Omega', 'Lambda', 'lambda']
+    for elem in tex_greek:
+        if elem in name:
+            position = name.find(elem)
+            name = name[:position]+"""\\"""+name[position:]
+    if name.find('_') != -1:
+        temp_name = name.split('_')[0]+'_{'
+        for i in range(1, len(name.split('_'))):
+            temp_name += name.split('_')[i]+' '
+        name = temp_name + '}'
+    if number == 1:
+        name = "${0}$".format(name)
+    elif (number < 1000 and number > 1):
+        name = "$%0.d~%s$" % (number, name)
+    else:
+        temp_name = "$%0.e%s$" % (number, name)
+        m = re.search(r'(?:\$[0-9]*e\+[0]*)([0-9]*)(.*)', temp_name)
+        sign = '+'
+        if m is None:
+            m = re.search(r'(?:\$[0-9]*e\-[0]*)([0-9]*)(.*)', temp_name)
+            sign = '-'
+        name = '$10^{'+sign+m.groups()[0]+'}'+m.groups()[1]
+    return name
+
 
 # New class of file, to provide an equivalent of the tail command (on linux).
 # It will be used when starting from an existing chain, and avoids circling
-# through an immense file. 
+# through an immense file.
 class File(file):
 
-  def tail(self, lines_2find=1):  
-    self.seek(0, 2)                         #go to end of file
-    bytes_in_file = self.tell()             
-    lines_found, total_bytes_scanned = 0, 0
-    while (lines_2find+1 > lines_found and
-       bytes_in_file > total_bytes_scanned): 
-      byte_block = min(1024, bytes_in_file-total_bytes_scanned)
-      self.seek(-(byte_block+total_bytes_scanned), 2)
-      total_bytes_scanned += byte_block
-      lines_found += self.read(1024).count('\n')
-    self.seek(-total_bytes_scanned, 2)
-    line_list = list(self.readlines())
-    return line_list[-lines_2find:]
+    def tail(self, lines_2find=1):
+        self.seek(0, 2)     # go to end of file
+        bytes_in_file = self.tell()
+        lines_found, total_bytes_scanned = 0, 0
+        while (lines_2find+1 > lines_found and
+                bytes_in_file > total_bytes_scanned):
+            byte_block = min(1024, bytes_in_file-total_bytes_scanned)
+            self.seek(-(byte_block+total_bytes_scanned), 2)
+            total_bytes_scanned += byte_block
+            lines_found += self.read(1024).count('\n')
+        self.seek(-total_bytes_scanned, 2)
+        line_list = list(self.readlines())
+        return line_list[-lines_2find:]
