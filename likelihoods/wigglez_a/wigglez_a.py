23267f13725e1996c5da57f03d9a17163d4e63f9
Benjamin Audren <benj_audren@yahoo.fr>
2013-08-13 18:19:54 +0200

    removed unnecessary wigglez structures

--- likelihoods/wigglez_a/wigglez_a.py
+++
@@ -1,6 +0,0 @@
-# likelihood for wigglez_a (first redshift bin)
-from likelihood_class import likelihood_mpk
-
-
-class wigglez_a(likelihood_mpk):
-    pass
--- likelihoods/wigglez_b/wigglez_b.py
+++
@@ -1,6 +0,0 @@
-# likelihood for wigglez_b (second redshift bin)
-from likelihood_class import likelihood_mpk
-
-
-class wigglez_b(likelihood_mpk):
-    pass
