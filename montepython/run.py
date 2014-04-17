9329c800311e8af3a416b59739f0a71a2e1c8b5d
Benjamin Audren <benjamin.audren@epfl.ch>
2014-04-17 10:57:38 +0200

    Mock implementation of covariance matrix update - never called

--- montepython/run.py
+++ montepython/run.py
@@ -5,8 +5,10 @@
 
 """
 from initialise import initialise
-import sys
 import io_mp
+import sys
+import warnings
+import os
 import re
 
 
@@ -68,8 +70,24 @@ def mpi_run(custom_command=""):
         cosmo, data, command_line, success = safe_initialisation(
             custom_command, comm, nprocs)
 
-        regexp = re.match(".*__(\w*).txt", data.out_name)
-        suffix = regexp.groups()[0]
+        # Check that the run asked is compatible with mpirun.
+        failed = False
+        if command_line.subparser_name == 'info':
+            warnings.warn(
+                "Analyzing the chains is not supported in mpirun"
+                " so this will run on one core only.")
+            failed = True
+        elif command_line.method in ['NS', 'CH', 'IS']:
+            warnings.warn(
+                "The methods NS, CH and IS are not supported in mpirun"
+                " so this will run on one core only.")
+            failed = True
+
+        if failed:
+            suffix = 'failed'
+        else:
+            regexp = re.match(".*__(\w*).txt", data.out_name)
+            suffix = regexp.groups()[0]
         # Send an "OK" signal to all the other processes, actually giving the
         # suffix of this master chain. All the other will add 1 to this number
         for index in range(1, nprocs):
@@ -81,18 +99,50 @@ def mpi_run(custom_command=""):
 
         # If a failed message was passed, exit the process
         if suffix == 'failed':
-            return
+            success = False
+        else:
+            # Concatenate the rank to the suffix, and not the opposite, this
+            # should avoid any conflicting name
+            if not custom_command:
+                custom_command = " ".join(sys.argv[1:])
+            custom_command += " --chain-number %s" % str(int(suffix)+rank)
+            cosmo, data, command_line, success = initialise(custom_command)
 
-        # Concatenate the rank to the suffix, and not the opposite, this should
-        # avoid any conflicting name
-        if not custom_command:
-            custom_command = " ".join(sys.argv[1:])
-        custom_command += " --chain-number %s" % str(int(suffix)+rank)
-        cosmo, data, command_line, success = initialise(custom_command)
+    if success:
+        import sampler
+        sampler.run(cosmo, data, command_line)
 
-    import sampler
-    sampler.run(cosmo, data, command_line)
+    success = comm.gather(success, root=0)
+    return
 
+
+def mock_update_run(custom_command=""):
+    """
+    Tentative covmat update run
+
+    Not reachable yet by any option.
+    """
+    from mpi4py import MPI
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+
+    # store the command_line space
+    if not custom_command:
+        custom_command = " ".join(sys.argv[1:])
+
+    # Do a first run
+    mpi_run(custom_command)
+
+    # Compute the covariance matrix
+    if rank == 0:
+        info_command = from_run_to_info(custom_command)
+        initialise(info_command)
+
+    # Make sure that the covariance matrix used is the one just computed
+    with_covmat = add_covariance_matrix(custom_command)
+
+    mpi_run(with_covmat)
     return
 
 
@@ -131,3 +181,70 @@ def safe_initialisation(custom_command="", comm=None, nprocs=1):
             "wrong likelihood, etc...). If you have solved the issue, you "
             "should remove completely the output folder, and try again.")
     return cosmo, data, command_line, success
+
+
+def from_run_to_info(command):
+    """
+    Translate a command corresponding to a run into one for analysis
+
+    """
+    original = command.split()
+    new = ['info']
+
+    # Recover the folder
+    index = original.index('-o')
+    folder = original[index+1]
+
+    # Recover chain number
+    index = original.index('-N')
+    number = original[index+1]
+
+    # Recover all relevant chains
+    chains = [os.path.join(folder, elem) for elem in os.listdir(folder)
+              if elem.find(number) != -1]
+    new.extend(chains)
+
+    # Do not plot the pdf
+    new.append("--noplot")
+    return " ".join(new)
+
+
+def add_covariance_matrix(command):
+    """
+    Make sure that the command uses the covariance matrix from the folder
+
+    """
+    original = command.split()
+
+    # recover the folder
+    index = original.index('-o')
+    folder = original[index+1]
+    # get the name of the folder
+    name = folder.split(os.path.sep)[-1]
+    covname = os.path.join(
+        folder, name+'.covmat')
+    bfname = os.path.join(
+        folder, name+'.bestfit')
+
+    if '-c' in original:
+        index = original.index('-c')
+        original[index+1] = covname
+    else:
+        original.extend(['-c', covname])
+
+    if '-b' in original:
+        index = original.index('-b')
+        original[index+1] = bfname
+    elif '--bestfit' in original:
+        index = original.index('--bestfit')
+        original[index+1] = bfname
+    else:
+        original.extend(['-b', bfname])
+
+    # Change the number of steps asked
+    index = original.index('-N')
+    number = list(str(original[index+1]))
+    number[0] = str(int(number[0])+1)
+    newnumber = "".join(number)
+    original[index+1] = str(newnumber)
+    return " ".join(original)
