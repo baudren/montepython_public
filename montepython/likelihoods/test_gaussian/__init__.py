2418203247dc5493da1b69c6858b348e570c4460
Jesus Torrado (LeidenUniv) <JesusTorrado@users.noreply.github.com>
2014-05-06 20:43:29 +0200

    NS: fixing test_gaussian likelihood I

--- montepython/likelihoods/test_gaussian/__init__.py
+++ montepython/likelihoods/test_gaussian/__init__.py
@@ -1,6 +1,5 @@
 from montepython.likelihood_class import Likelihood
-from numpy import matrix, dot
-
+from numpy import matrix, dot, exp, log
 
 class test_gaussian(Likelihood):
 
@@ -9,11 +8,11 @@ class test_gaussian(Likelihood):
             data.mcmc_parameters[p]['current']*data.mcmc_parameters[p]['scale']
             for p in ['H0', 'omega_b', 'omega_cdm'])
         # Modes
-        loglkl = 0
+        lkl = 0
         for centre, covmat in zip([self.centre1, self.centre2, self.centre3],
                                   [self.covmat1, self.covmat2, self.covmat3]):
             diffvec = matrix([x-mu for x, mu in zip([H0, ob, oc], centre)])
-            Hessian = matrix(covmat).I
-            loglkl += -0.5 * (dot(diffvec, dot(Hessian, diffvec.T)))
-        return loglkl
+            minusHessian = matrix(covmat).I
+            lkl += exp(-0.5 * (dot(diffvec, dot(minusHessian, diffvec.T))))
+        return log(lkl)
 
