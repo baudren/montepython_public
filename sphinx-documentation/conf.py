1b5f138349bae96180988e09985d20f6cec0fea9
Julien Lesgourgues <lesgourg@cern.ch>
2015-10-21 15:43:28 +0200

    updated sphinx configuration file for sphinx 1.3

--- sphinx-documentation/conf.py
+++ sphinx-documentation/conf.py
@@ -41,7 +41,7 @@ sys.path.insert(-1, os.path.abspath('../montepython'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode',
-'sphinx.ext.autosummary','sphinx.ext.mathjax', 'sphinxcontrib.napoleon']
+'sphinx.ext.autosummary','sphinx.ext.mathjax', 'sphinx.ext.napoleon']
 #,'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
